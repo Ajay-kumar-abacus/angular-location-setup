228a4a14495c1935fb80982c36906eb3b481703f
Ajaya kumar Behura <144202352+Ajay-kumar-abacus@users.noreply.github.com>
2025-11-26 21:43:10 +0530

    Add files via upload

---
+++ setup-files/map/map.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule, MatDialogModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { AppUtilityModule } from 'src/app/app-utility.module';
+import { MaterialModule } from 'src/app/material';
+import { AuthComponentGuard } from 'src/app/auth-component.guard';
+
+
+import { MapComponent } from './map.component';
+
+const mapRoutes = [
+  {path:"", children:[
+    { path: "", component:MapComponent,canActivate:[AuthComponentGuard], data:{ expectedRole: ['1']}},
+
+  ]}
+] 
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(mapRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    NgMultiSelectDropDownModule,
+    MaterialModule,
+    AutocompleteLibModule,
+    MatIconModule,
+    MatDialogModule,
+    NgxMatSelectSearchModule,
+    AppUtilityModule
+  ],
+  entryComponents:[
+  ]
+})
+export class MapModule { }
+
